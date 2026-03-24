b0d329f87800584032fe952d717a801328305666
RogueJet18 <mollyelkes@gmail.com>
2026-03-24 16:26:30 +0000

    small corrections
    enemy still gets stuck but at least the sprite flips properly now

--- game_jam_4_v_0.01/levels/scenes/enemy.gd
+++ game_jam_4_v_0.01/levels/scenes/enemy.gd
@@ -1,16 +1,21 @@
 extends CharacterBody2D
 
 @export var speed := 50 # how fast it moves
-
+@onready var ray_cast: RayCast2D = $RayCast2D
+@onready var enemy_sprite: Sprite2D = $Sprite2D
 var direction := -1
 
 func _physics_process(_delta):
 	
 	# for detect edge walking
-	if $RayCast2D.is_colliding() == false:
+	if not ray_cast.is_colliding():
 		direction *= -1 # flip direction
-		$RayCast2D.position.x *= -1 # flip raycast2d direction
-		$Sprite2D.flip_h = direction > 0 # flip sprite
+		ray_cast.position.x *= -1 # flip raycast2d direction
+		enemy_sprite.flip_h = true # flip sprite
+	else:
+		direction *= 1
+		ray_cast.position.x *= 1 # flip raycast2d direction
+		enemy_sprite.flip_h = false
 	
 	# move left/right
 	velocity.x = direction * speed
